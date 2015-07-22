691510e282e1a7d5bc141f63bb37d06a48de8e44
Sarah Story <sarah.f.lavendol@vanderbilt.edu>
2015-07-22 13:53:46 -0500

    Switched to Django

---
+++ on_the_line/orders/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('items', models.TextField()),
+                ('order_time', models.DateTimeField()),
+            ],
+        ),
+    ]
---
+++ on_the_line/orders/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from bottle import post, request, run
+from hashlib import sha1
+from models.py import Order
+import hmac, httplib, json, locale, datetime
+
+# Create your views here.
+def get_orders(request):
+    access_token = 'b0y7fYe4mEiskcVybKylkw'
+    request_headers = { 'Authorization': 'Bearer ' + access_token,
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'}
+    callback_body = request.body.getvalue()
+    callback_body_dict = json.loads(callback_body)
+    payment_id = callback_body_dict['entity_id']
+    connection = httplib.HTTPSConnection('connect.squareup.com')
+    connection.request('GET', '/v1/me/payments/' + payment_id, '', request_headers)
+    connect_response_body = json.loads(connection.getresponse().read())
+    items_information = connect_response_body['itemizations']
+    items_list = []
+    for item in items_information:
+        items_list += str(round(item['quantity'])) + " " + item['name'] + "<br>"
+    i = Order(items = items_list, order_time = datetime.datetime.now())
+    i.save()
+    return
+
+
+
+
+
