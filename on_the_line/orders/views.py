c0a0dec2454683d99c8041b81bdb5aaf4b5910ea
Sarah Story <sarah.f.lavendol@vanderbilt.edu>
2015-07-23 10:47:14 -0500

    debugging

--- on_the_line/orders/views.py
+++ on_the_line/orders/views.py
@@ -8,22 +8,22 @@ from django.http import HttpResponse
 
 @csrf_exempt
 def get_orders(request):
-    access_token = 'b0y7fYe4mEiskcVybKylkw'
-    request_headers = { 'Authorization': 'Bearer ' + access_token,
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'}
-    callback_body = request.body.getvalue()
-    callback_body_dict = json.loads(callback_body)
-    payment_id = callback_body_dict['entity_id']
-    connection = httplib.HTTPSConnection('connect.squareup.com')
-    connection.request('GET', '/v1/me/payments/' + payment_id, '', request_headers)
-    connect_response_body = json.loads(connection.getresponse().read())
-    items_information = connect_response_body['itemizations']
-    items_list = []
-    for item in items_information:
-        items_list += str(round(item['quantity'])) + " " + item['name'] + "<br>"
-    i = Order(items = items_list, order_time = datetime.datetime.now())
-    i.save()
+    # access_token = 'b0y7fYe4mEiskcVybKylkw'
+    # request_headers = { 'Authorization': 'Bearer ' + access_token,
+    #                     'Accept': 'application/json',
+    #                     'Content-Type': 'application/json'}
+    # callback_body = request.body.getvalue()
+    # callback_body_dict = json.loads(callback_body)
+    # payment_id = callback_body_dict['entity_id']
+    # connection = httplib.HTTPSConnection('connect.squareup.com')
+    # connection.request('GET', '/v1/me/payments/' + payment_id, '', request_headers)
+    # connect_response_body = json.loads(connection.getresponse().read())
+    # items_information = connect_response_body['itemizations']
+    # items_list = []
+    # for item in items_information:
+    #     items_list += str(round(item['quantity'])) + " " + item['name'] + "<br>"
+    # i = Order(items = items_list, order_time = datetime.datetime.now())
+    # i.save()
     return HttpResponse("")
 
 
